a413b973dfbf1167c4226a3727ccf7111e326186
Krishan Kulasinghe <kpkrishpri@gmail.com>
2023-03-11 02:03:27 -0500

    .gitignore is now working

---
+++ XamarinFirebaseApp/XamarinFirebaseApp/XamarinFirebaseApp/StudentRepository.cs
@@ -0,0 +1,58 @@
+﻿using Firebase.Database;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+
+namespace XamarinFirebaseApp
+{
+    public class ConfigFirebase
+    {
+        public static string FirebaseClient = "https://xamarinfirebase-e9288-default-rtdb.firebaseio.com/";
+    }
+    public class StudentRepository
+    {
+        FirebaseClient firebaseClient = new FirebaseClient(ConfigFirebase.FirebaseClient);
+        public async Task<bool> Save(StudentModel student)
+        {
+            try
+            {
+                var data = await firebaseClient.Child("StudentModel").PostAsync(JsonConvert.SerializeObject(student));
+                if (!string.IsNullOrEmpty(data.Key))
+                {
+                    return true;
+                }
+                return false;
+            }
+            catch (Exception e)
+            {
+                throw;
+            }
+        }
+
+        public async Task<List<StudentModel>> GetAll()
+        {
+            try
+            {
+                return (await firebaseClient.Child(nameof(StudentModel)).OnceAsync<StudentModel>()).Select(item => new StudentModel
+                {
+                    Email = item.Object.Email,
+                    Id = item.Key,
+                    Name = item.Object.Name,
+                    Image = item.Object.Image,
+                }).ToList();
+            }
+            catch (Exception e)
+            {
+
+
+            }
+
+            return new List<StudentModel>();
+        }
+    }
+}
---
+++ XamarinFirebaseApp/XamarinFirebaseApp/XamarinFirebaseApp/Views/Student/StudentListPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinFirebaseApp.Views.Student
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class StudentListPage : ContentPage
+    {
+        StudentRepository repository = new StudentRepository();
+        public StudentListPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override async void OnAppearing()
+        {
+           var students = await repository.GetAll();
+            StudentListView.ItemsSource = students;
+        }
+
+        private void AddToolBarItem_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushModalAsync(new StudentEntry());
+        }
+    }
+}
